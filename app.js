7afc2f4f0e7b86c5ace6c1cd162e12f6a3e3cba1
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-06-08 20:53:45 +0100

    finish scatterplot

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ async function draw() {
         }
     }
 
-    // Save container dimensions
     dimensions.ctrWidth= dimensions.width - dimensions.margin.left - dimensions.margin.right;
     dimensions.ctrHeight= dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
 
@@ -29,32 +28,55 @@ async function draw() {
             `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
 
     // Scales
-    // Map input value (domain) to output value (range)
     const xScale = d3.scaleLinear()
-        // extent() has an optional second argument for accessor function
-        // apply the xAccessor function to each data point in dataset
-        // to find the minimum and maximum humidity values
         .domain(d3.extent(dataset, xAccessor))
-        // here we can use 0 because we have margins
-        .range([0, dimensions.ctrWidth])
-
+        .rangeRound([0, dimensions.ctrWidth])
+        .clamp(true)
     const yScale = d3.scaleLinear()
         .domain(d3.extent(dataset, yAccessor))
-        .range([0, dimensions.ctrHeight])
+        .rangeRound([dimensions.ctrHeight, 0])
+        .nice()
+        .clamp(true)
 
+    // Draw circles
     ctr.selectAll('circle')
         .data(dataset)
         .join('circle')
-        // The scale expects a number, not an object
-        // Passing the humidity value extracted from the data point d into the scale function
-        // which then maps this humidity value to a corresponding position on the x-axis
-        // Or in other words:
-        // for each data point d, use the xAccessor function to retrieve the x-value
-        // and then use the xScale function to map this value on the x-axis
         .attr('cx', d => xScale(xAccessor(d)))
         .attr('cy', d => yScale(yAccessor(d)))
         .attr('r', 5)
         .attr('fill', 'red')
+        .attr('data-temp', yAccessor)
+
+    // Axes
+    const xAxis = d3.axisBottom(xScale)
+        .ticks(5)
+        .tickFormat((d) => d * 100 + '%')
+
+    const yAxis = d3.axisLeft(yScale)
+
+    const xAxisGroup = ctr.append('g')
+        .call(xAxis)
+        .style('transform', `translateY(${dimensions.ctrHeight}px)`)
+        .classed('axis', true)
+
+    xAxisGroup.append('text')
+        .attr('x', dimensions.ctrWidth / 2)
+        .attr('y', dimensions.margin.bottom - 10)
+        .attr('fill', 'black')
+        .text('Humidity')
+
+    const yAxisGroup = ctr.append('g')
+        .call(yAxis)
+        .classed('axis', true)
+
+    yAxisGroup.append('text')
+        .style('transform', `rotate(270deg)`)
+        .attr('x', -dimensions.ctrHeight / 2)
+        .attr('y', -dimensions.margin.left + 15)
+        .attr('fill', 'black')
+        .html('Apparent Temperature &deg; F')
+        .style('text-anchor', 'middle')
 }
 
 draw()
