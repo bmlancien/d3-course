e704290d9540a0ffad5af2e41df41df64a03cc9d
Bryan Lancien <bryanlancien.ui@gmail.com>
2024-06-08 17:24:24 +0100

    scale data points

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 async function draw() {
     const dataset = await d3.json('data.json')
+    const xAccessor = (d) => d.currently.humidity;
+    const yAccessor = (d) => d.currently.apparentTemperature;
 
     let dimensions = {
         width: 800,
         height: 800,
-        // add margins
-        // using 50 we don't have to worry about shaped that are smaller than 50 px
         margin: {
             top: 50,
             bottom: 50,
@@ -14,25 +14,47 @@ async function draw() {
         }
     }
 
+    // Save container dimensions
+    dimensions.ctrWidth= dimensions.width - dimensions.margin.left - dimensions.margin.right;
+    dimensions.ctrHeight= dimensions.height - dimensions.margin.top - dimensions.margin.bottom;
+
     const svg = d3.select('#chart')
         .append('svg')
         .attr('width', dimensions.width)
         .attr('height', dimensions.height)
 
-        // add group
-        // we can then apply properties the group
-        // which will affect all children
-        // But g doesn't accept x and y attributes
     const ctr = svg.append('g')
-        // We can use transform instead of x and y
-        // Works like CSS
-        // Could also be used as "style"
         .attr(
             'transform',
             `translate(${dimensions.margin.left}, ${dimensions.margin.top})`)
 
-    ctr.append('circle')
-        .attr('r', 15)
+    // Scales
+    // Map input value (domain) to output value (range)
+    const xScale = d3.scaleLinear()
+        // extent() has an optional second argument for accessor function
+        // apply the xAccessor function to each data point in dataset
+        // to find the minimum and maximum humidity values
+        .domain(d3.extent(dataset, xAccessor))
+        // here we can use 0 because we have margins
+        .range([0, dimensions.ctrWidth])
+
+    const yScale = d3.scaleLinear()
+        .domain(d3.extent(dataset, yAccessor))
+        .range([0, dimensions.ctrHeight])
+
+    ctr.selectAll('circle')
+        .data(dataset)
+        .join('circle')
+        // The scale expects a number, not an object
+        // Passing the humidity value extracted from the data point d into the scale function
+        // which then maps this humidity value to a corresponding position on the x-axis
+        // Or in other words:
+        // for each data point d, use the xAccessor function to retrieve the x-value
+        // and then use the xScale function to map this value on the x-axis
+        .attr('cx', d => xScale(xAccessor(d)))
+        .attr('cy', d => yScale(yAccessor(d)))
+        .attr('r', 5)
+        .attr('fill', 'red')
 }
 
 draw()
